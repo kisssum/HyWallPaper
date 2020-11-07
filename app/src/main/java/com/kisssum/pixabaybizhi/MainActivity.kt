7c3cd05e4a0925cb981ac8a993cb020e32fb888c
sea <1284700369@qq.com>
2020-11-07 15:58:50 +0800

    implement:onBackPressed() for MainActivity.kt

--- app/src/main/java/com/kisssum/pixabaybizhi/MainActivity.kt
+++ app/src/main/java/com/kisssum/pixabaybizhi/MainActivity.kt
@@ -3,22 +3,25 @@ package com.kisssum.pixabaybizhi
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Toast
+import androidx.navigation.findNavController
 import com.bumptech.glide.Glide
 import com.kisssum.pixabaybizhi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    var binding: ActivityMainBinding? = null
+    private var binding: ActivityMainBinding? = null
+    private var backTime: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
 
+        // 初始化Glide
         Glide.get(this)
         initUi()
-        initNavigationView()
     }
 
     private fun initNavigationView() {
@@ -36,5 +39,25 @@ class MainActivity : AppCompatActivity() {
     private fun initUi() {
         window.statusBarColor = Color.TRANSPARENT
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+
+        initNavigationView()
+    }
+
+    override fun onBackPressed() {
+        findNavController(R.id.fragment).let {
+            when (it.currentDestination!!.id) {
+                R.id.homeFragment -> {
+                    if (backTime + 2000 < System.currentTimeMillis()) {
+                        backTime = System.currentTimeMillis()
+
+                        val toast = Toast.makeText(this, "在按一次退出程序", Toast.LENGTH_SHORT)
+                        toast.setGravity(Gravity.CENTER, 0, 0)
+                        toast.show()
+                    } else
+                        super.onBackPressed()
+                }
+                else -> it.popBackStack()
+            }
+        }
     }
 }
