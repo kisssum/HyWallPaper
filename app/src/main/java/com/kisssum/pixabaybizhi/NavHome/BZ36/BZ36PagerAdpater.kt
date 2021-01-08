ca768e80d2c2e9940fc0fad451a065b307e28487
sea <1284700369@qq.com>
2021-01-08 21:39:10 +0800

    在手机壁纸页用正则替代jsoup爬虫

--- app/src/main/java/com/kisssum/pixabaybizhi/NavHome/BZ36/BZ36PagerAdpater.kt
+++ app/src/main/java/com/kisssum/pixabaybizhi/NavHome/BZ36/BZ36PagerAdpater.kt
@@ -13,7 +13,7 @@ import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.kisssum.pixabaybizhi.R
-import org.jsoup.Jsoup
+import rxhttp.wrapper.param.RxHttp
 
 class BZ36PagerAdpater(private val context: Context, private val typeIndex: Int) :
     RecyclerView.Adapter<BZ36PagerAdpater.MyViewHolder>() {
@@ -109,34 +109,28 @@ class BZ36PagerAdpater(private val context: Context, private val typeIndex: Int)
             else -> "index_${page}.html"
         }
 
-        Thread {
-            try {
-                val doc = Jsoup.connect(url).get()
-                val esa = when {
-                    (page == 1 && typeIndex == 0) -> doc.select("body > div:nth-child(6) > ul > li > a")
-                    (page != 1 && typeIndex == 0) -> doc.select("body > div:nth-child(5) > ul > li > a")
-                    else -> doc.select("body > div.contlistw.mtw > ul > li > a")
-                }
+        RxHttp.get(url)
+            .add("User-Agent",
+                "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.142 Safari/537.36")
+            .asString()
+            .subscribe({
+                val href = """<a href="(.+?)".*?imgw.*?>""".toRegex().findAll(it)
+                val lazysrc = """lazysrc="(.+?)"""".toRegex().findAll(it)
+                val lazysrc2x = """lazysrc2x="(.+?) 2x"""".toRegex().findAll(it)
 
                 val list = arrayListOf<Map<String, String>>()
-                var map: HashMap<String, String>
-                for (e in esa) {
-                    val lazyimg = e.select("img")
-
-                    map = HashMap()
-                    map["href"] = e.attr("href")
-                    map["lazysrc"] = lazyimg.attr("lazysrc")
-                    map["lazysrc2x"] = lazyimg.attr("lazysrc2x").split(" ")[0]
+                for (i in 0 until (href.count() - 1)) {
+                    val map = hashMapOf<String, String>()
+                    map["href"] = href.toList()[i].destructured.component1()
+                    map["lazysrc"] = lazysrc.toList()[i].destructured.component1()
+                    map["lazysrc2x"] = lazysrc2x.toList()[i].destructured.component1()
                     list.add(map)
                 }
 
                 val message = Message.obtain()
-                message.obj = list
                 if (upgrad) message.what = 1 else message.what = 2
+                message.obj = list
                 handler.sendMessage(message)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }.start()
+            }, {})
     }
 }
