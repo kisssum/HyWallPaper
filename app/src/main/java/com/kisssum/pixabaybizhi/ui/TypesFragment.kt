6c779dbe647d198deffa5312330a9bda555c3127
sea <1284700369@qq.com>
2021-02-16 13:57:34 +0800

    task1:new build TypesFragment.kt

--- app/src/main/java/com/kisssum/pixabaybizhi/ui/TypesFragment.kt
+++ app/src/main/java/com/kisssum/pixabaybizhi/ui/TypesFragment.kt
@@ -1,6 +1,5 @@
-package com.kisssum.pixabaybizhi.NavMain
+package com.kisssum.pixabaybizhi.ui
 
-import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import com.kisssum.pixabaybizhi.R
-import com.kisssum.pixabaybizhi.databinding.FragmentHomeBinding
+import com.kisssum.pixabaybizhi.databinding.FragmentTypesBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,14 +16,15 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
+ * Use the [TypesFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeFragment : Fragment() {
+class TypesFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private lateinit var binding: FragmentHomeBinding
+
+    private lateinit var binding: FragmentTypesBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,39 +38,16 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        binding = FragmentHomeBinding.inflate(inflater)
+        binding = FragmentTypesBinding.inflate(inflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.bottomNavigationView.setOnNavigationItemSelectedListener {
-            val nav = Navigation.findNavController(requireActivity(), R.id.fragment_home)
-
-            when (it.itemId) {
-                R.id.Item_master -> {
-                    if (nav.currentDestination?.id != R.id.masterFragment) nav.navigate(R.id.masterFragment)
-                }
-                R.id.Item_types -> {
-                    if (nav.currentDestination?.id != R.id.typesFragment) nav.navigate(R.id.typesFragment)
-                }
-                R.id.Item_bian -> {
-                    if (nav.currentDestination?.id != R.id.bianMainFragment) nav.navigate(R.id.bianMainFragment)
-                }
-                R.id.Item_pix -> {
-                    if (nav.currentDestination?.id != R.id.pixabayMainFragment) nav.navigate(R.id.pixabayMainFragment)
-                }
-                R.id.Item_bz36 -> {
-                    if (nav.currentDestination?.id != R.id.BZ36MainFragment) nav.navigate(R.id.BZ36MainFragment)
-                }
-//                R.id.Item_me -> {
-//                    if (nav.currentDestination?.id != R.id.meFragment) nav.navigate(R.id.meFragment)
-//                }
-                else -> true
-            }
-
-            true
+        binding.typesSearch.searchBorder.setOnClickListener {
+            val controller = Navigation.findNavController(requireActivity(), R.id.fragment_main)
+            controller.navigate(R.id.action_homeFragment_to_searchFragment)
         }
     }
 
@@ -81,12 +58,12 @@ class HomeFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
+         * @return A new instance of fragment TypesFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
+            TypesFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
